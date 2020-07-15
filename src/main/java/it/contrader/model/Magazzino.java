a7980a1a34d9f358eaab7786a9b1ecfa1cfe3d8a
jOh <j0h@example.com>
2020-07-15 16:29:06 +0200

    aggiornato magazzino

--- src/main/java/it/contrader/model/Magazzino.java
+++ src/main/java/it/contrader/model/Magazzino.java
@@ -5,6 +5,7 @@ private int id;
 private int id_oggetto;
 private int capienza;
 private int posizione;
+private int codice ;
 public Magazzino(int id_oggetto, int capienza, int posizione) {
 	this.id_oggetto= id_oggetto;
 	this.capienza= capienza;
@@ -16,6 +17,14 @@ public Magazzino(int id, int id_oggetto, int capienza, int posizione) {
 	this.id_oggetto= id_oggetto;
 	this.capienza= capienza;
 	this.posizione= posizione;
+}
+
+public int getCodice() {
+	return codice;
+}
+
+public int SetCodice (int codice) {
+	this.codice=codice; 
 }
 
 public int getId() {
