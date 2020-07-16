149a065bf99c9990b2854411cc0d88fccaf1aaaa
Alessio Guglielmi <alessio.guglielmi.1990@gmail.com>
2020-07-16 11:03:42 +0200

    Corretto CodiceDTO

--- src/main/java/it/contrader/dto/CodiceDTO.java
+++ src/main/java/it/contrader/dto/CodiceDTO.java
@@ -3,29 +3,19 @@ package it.contrader.dto;
 public class CodiceDTO {
 private int id;
 private int codice;
-<<<<<<< HEAD
 public CodiceDTO(int id, int codice){
 	this.id = id;
 	this.codice = codice; 
 }
 
 public CodiceDTO(int codice){
-=======
-public CodiceDTO(String nome, int codice){
-	this.id= id;
-	this.codice = codice; 
-}
-
-public CodiceDTO(int id, int dimensione, int codice){
-	this.id = id;
->>>>>>> 0ace919cf34d1162062a0b0f11dc68a864900b94
 	this.codice = codice; 
 }
 
 public int getId() {
 	return id;
 }
-<<<<<<< HEAD
+
 public void setId(int id) {
 	this.id = id;
 }
@@ -35,12 +25,6 @@ public int getCodice() {
 public void setCodice(int codice) {
 	this.codice=codice;
 }
-=======
-public void setCodice(int id) {
-	this.codice = id;
-}
-
->>>>>>> 0ace919cf34d1162062a0b0f11dc68a864900b94
 
 
 }
