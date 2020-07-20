1e5ac5442274de75d5cfb09076501b278926dada
Ruggiero <r.dimiccoli@yahoo.it>
2020-07-20 14:57:59 +0200

    Oggetto e Magazzino Service

---
+++ src/main/java/it/contrader/service/MagazzinoService.java
@@ -0,0 +1,62 @@
+package it.contrader.service;
+import java.sql.SQLException;
+import java.util.List;
+
+import it.contrader.converter.MagazzinoConverter;
+import it.contrader.dao.MagazzinoDAO;
+import it.contrader.dto.MagazzinoDTO;
+
+
+public class MagazzinoService {
+	private MagazzinoDAO magazzinoDAO;
+	private MagazzinoConverter magazzinoConverter;
+	
+	//Istanzio DAO  e Converter specifici.
+	public MagazzinoService(){
+		this.magazzinoDAO = new MagazzinoDAO();
+		this.magazzinoConverter = new MagazzinoConverter();
+	}
+	
+
+	public List<MagazzinoDTO> getAll() {
+		// Ottiene una lista di entit� e le restituisce convertendole in DTO
+		return magazzinoConverter.toDTOList(magazzinoDAO.getAll());
+	}
+
+
+	public MagazzinoDTO read(int id) {
+		// Ottiene un'entit� e la restituisce convertendola in DTO
+		return magazzinoConverter.toDTO(magazzinoDAO.read(id));
+	}
+
+
+	public boolean insert(MagazzinoDTO dto) {
+		// Converte un DTO in entit� e lo passa al DAO per l'inserimento
+		return magazzinoDAO.insert(magazzinoConverter.toEntity(dto));
+	}
+
+
+	public boolean update(MagazzinoDTO dto) {
+		// Converte un magazzinoDTO in entit� e lo passa allo magazzinoDAO per la modifica
+		return magazzinoDAO.update(magazzinoConverter.toEntity(dto));
+	}
+
+
+	public boolean delete(int id) {
+		// Questo mtodo chiama direttamente il DAO
+		return magazzinoDAO.delete(id);
+	}
+	
+	public int checkOggetto(int id) {
+		return magazzinoDAO.checkOggetto(id);
+	}
+	
+	public boolean removeOggetto(int oggetto) {
+		return magazzinoDAO.removeOggetto(oggetto);
+	}
+	
+	public int checkCodice(int id) throws SQLException {
+		return magazzinoDAO.checkCodice(id);
+	}
+
+}
---
+++ src/main/java/it/contrader/service/OggettoService.java
@@ -0,0 +1,61 @@
+package it.contrader.service;
+
+import java.util.List;
+
+import it.contrader.converter.OggettoConverter;
+import it.contrader.dao.OggettoDAO;
+import it.contrader.dto.OggettoDTO;
+
+public class OggettoService {
+	private OggettoDAO oggettoDAO;
+	private OggettoConverter oggettoConverter;
+	
+	//Istanzio DAO  e Converter specifici.
+	public OggettoService(){
+		this.oggettoDAO = new OggettoDAO();
+		this.oggettoConverter = new OggettoConverter();
+	}
+	
+
+	public List<OggettoDTO> getAll() {
+		// Ottiene una lista di entit� e le restituisce convertendole in DTO
+		return oggettoConverter.toDTOList(oggettoDAO.getAll());
+	}
+
+
+	public OggettoDTO read(int id) {
+		// Ottiene un'entit� e la restituisce convertendola in DTO
+		return oggettoConverter.toDTO(oggettoDAO.read(id));
+	}
+
+
+	public boolean insert(OggettoDTO dto) {
+		// Converte un DTO in entit� e lo passa al DAO per l'inserimento
+		return oggettoDAO.insert(oggettoConverter.toEntity(dto));
+	}
+
+
+	public boolean update(OggettoDTO dto) {
+		// Converte un magazzinoDTO in entit� e lo passa allo magazzinoDAO per la modifica
+		return oggettoDAO.update(oggettoConverter.toEntity(dto));
+	}
+
+
+	public boolean delete(int id) {
+		// Questo mtodo chiama direttamente il DAO
+		return oggettoDAO.delete(id);
+	}
+	
+	public int id(int id) {
+		return oggettoDAO.id(id);
+	}
+	
+	public int dimensione(int id) {
+		return oggettoDAO.dimensione(id);
+	}
+	
+	public List<OggettoDTO> getAllInCell() {
+		// Ottiene una lista di entit� e le restituisce convertendole in DTO
+		return oggettoConverter.toDTOList(oggettoDAO.getAllInCell());
+	}
+}
