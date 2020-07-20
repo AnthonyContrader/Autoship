76d06a10eb3a7af73fe821e05d4879b0d25f7b83
Alessio Guglielmi <alessio.guglielmi.1990@gmail.com>
2020-07-20 14:19:51 +0200

    Risolta la connessione con il DB e aggiustata la vista del User

--- src/main/java/it/contrader/utils/ConnectionSingleton.java
+++ src/main/java/it/contrader/utils/ConnectionSingleton.java
@@ -3,7 +3,6 @@ package it.contrader.utils;
 import java.sql.Connection;
 import java.sql.DriverManager;
 
-
 public class ConnectionSingleton {
 
 
@@ -18,18 +17,18 @@ public class ConnectionSingleton {
         if (connection == null) {
             try {
                 String vendor="mysql";
-                String driver="com.mysql.cj.jdbc.Driver";
+                String driver="com.mysql.jdbc.Driver";
                 String host="127.0.0.1";
                 String port="3306";
                 String dbName="sampledb";
                 String username="root";
-                String password ="root";
+                String password ="rootroot";
                 String jdbcAdditionalParams="useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC&useLegacyDatetimeCode=false";
                 Class<?> c = Class.forName(driver);
                 System.out.println("Ho caricato: " + c.getName());
                 String url = "jdbc:" + vendor + "://" + host + ":" + port + "/" + dbName+"?"+jdbcAdditionalParams;
                 connection = (Connection) DriverManager.getConnection(url, username, password);
-               // DriverManagerDataSource dataSource = new DriverManagerDataSource(myUrl, username, password);
+                //DriverManagerDataSource dataSource = new DriverManagerDataSource(myUrl, username, password);
                 //dataSource.setDriverClassName(driver);
                 //connection = dataSource.getConnection();
             } catch (Exception e) {
