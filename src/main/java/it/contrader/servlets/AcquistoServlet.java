3b30b3497752da35c2589f42888e1c7c971bc8b7
Alessio Guglielmi <alessio.guglielmi.1990@gmail.com>
2020-07-21 09:42:56 +0200

    Creato scheletro per la gestione dell'Utente

---
+++ src/main/java/it/contrader/servlets/AcquistoServlet.java
@@ -0,0 +1,12 @@
+package it.contrader.servlets;
+
+import javax.servlet.http.HttpServlet;
+
+public class AcquistoServlet extends HttpServlet {
+	
+	private static final long serialVersionUID = 1L;
+
+	public AcquistoServlet() {
+	}
+
+}
