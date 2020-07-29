c6cb489717e531f7ea4561e92dd3369c38dacbc8
Ruggiero <r.dimiccoli@yahoo.it>
2020-07-29 10:05:16 +0200

    Implementato CarrelloConverter

--- src/main/java/it/contrader/converter/CarrelloConverter.java
+++ src/main/java/it/contrader/converter/CarrelloConverter.java
@@ -3,9 +3,28 @@ package it.contrader.converter;
 import org.springframework.stereotype.Component;
 
 import it.contrader.dto.CarrelloDTO;
+
 import it.contrader.model.Carrello;
 
-@Component
-public class CarrelloConverter extends AbstractConverter<Carrello, CarrelloDTO>{
 
+@Component
+public class CarrelloConverter extends AbstractConverter<Carrello, CarrelloDTO> {
+	public Carrello toEntity(CarrelloDTO carrelloDTO) {
+		Carrello carrello = null;
+		if (carrello != null) {
+			carrello = new Carrello(carrelloDTO.getId(), carrelloDTO.getUser(), carrelloDTO.getOggetto(), carrelloDTO.getCodice(), carrello.getStato());
+		}
+		return carrello ;
+	}
+
+	@Override
+	public CarrelloDTO toDTO(Carrello carrello) {
+		CarrelloDTO carrelloDTO = null;
+		if (carrello != null) {
+			carrelloDTO = new CarrelloDTO(carrello.getId(), carrello.getUser(), carrello.getOggetto(), carrello.getCodice(), carrello.getStato());
+
+		}
+		return carrelloDTO;
+	}
 }
+	
