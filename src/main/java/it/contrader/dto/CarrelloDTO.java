8b2fb08d4e955262622bc5bb2ce96f4b5f3446bd
Ruggiero <r.dimiccoli@yahoo.it>
2020-07-29 09:49:52 +0200

    Creato CarrelloDTO

--- src/main/java/it/contrader/dto/CarrelloDTO.java
+++ src/main/java/it/contrader/dto/CarrelloDTO.java
@@ -1,5 +1,9 @@
 package it.contrader.dto;
 
+import it.contrader.model.Codice;
+import it.contrader.model.Oggetto;
+import it.contrader.model.User;
+import it.contrader.model.Carrello.CarrelloStato;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -9,5 +13,15 @@ import lombok.NoArgsConstructor;
 @Data
 
 public class CarrelloDTO {
+	
+	private Long id;
+	
+	private User user;
 
+	private Oggetto oggetto;
+		
+	private Codice codice;
+		
+	private CarrelloStato stato;
 }
+
