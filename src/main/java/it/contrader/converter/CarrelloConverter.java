c21a6c54e0b0a3fc2abad24d8bae8784f34915b3
Alessio Guglielmi <alessio.guglielmi.1990@gmail.com>
2020-07-29 09:54:32 +0200

    Creato file CarrelloConverter

---
+++ src/main/java/it/contrader/converter/CarrelloConverter.java
@@ -0,0 +1,11 @@
+package it.contrader.converter;
+
+import org.springframework.stereotype.Component;
+
+import it.contrader.dto.CarrelloDTO;
+import it.contrader.model.Carrello;
+
+@Component
+public class CarrelloConverter extends AbstractConverter<Carrello, CarrelloDTO>{
+
+}
