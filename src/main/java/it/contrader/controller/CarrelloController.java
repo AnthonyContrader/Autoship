352e238357096b1260d77ba6034ccde491b89947
jOh <j0h@example.com>
2020-07-29 10:06:44 +0200

    ciaone

--- src/main/java/it/contrader/controller/CarrelloController.java
+++ src/main/java/it/contrader/controller/CarrelloController.java
@@ -7,4 +7,26 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/carrello")
 public class CarrelloController {
 
+	@Autowired
+	private CarrelloService service;
+	
+	
+	@Autowired
+	private CarrelloConverter converter;
+	
+	
+	
+	@GetMapping("/getall")
+	public String getAll(HttpServletRequest request) {
+		
+	setAll(request) ;
+	return"carrello";
+	}
+	
+	
+	private void setAll(HttpServletRequest request) {
+		UserDTO userDTO =request.getSession().getAttribute("user");
+		request.setAttribute("list", service.findByUser(converter.toEntity(userDTO)));
+	}
+	
 }
