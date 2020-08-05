9fdcbc697b35914b98d360361928aa2dec61a8ee
Mario Trentino <mario31@MacBook-Air-di-Mario.local>
2020-08-05 18:04:00 +0200

    Modificato oggetto controller

--- src/main/java/it/contrader/controller/OggettoController.java
+++ src/main/java/it/contrader/controller/OggettoController.java
@@ -6,6 +6,7 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -43,8 +44,8 @@ public class OggettoController extends AbstractController<OggettoDTO>{
 		oggetto.setCancellato(true);
 		return service.update(oggetto);
 	}
-	
-	private List<OggettoDTO> getObjectNotInCell(HttpServletRequest request) {
+	@GetMapping("/getobjectnotincell")
+	public List<OggettoDTO> getObjectNotInCell(HttpServletRequest request) {
 		List<OggettoDTO> oggettoList = service.findByCancellatoFalse();
 		List<OggettoDTO> dummyList = service.findByCancellatoFalse();
 		MagazzinoDTO magazzino;
