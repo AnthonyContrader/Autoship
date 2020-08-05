431dafab5eb8b0bbdfdd801c66360801745a7ead
Alessio Guglielmi <alessio.guglielmi.1990@gmail.com>
2020-08-05 05:39:13 +0200

    Fix d'Utente

--- src/app/admin/general-info/general-info.component.ts
+++ src/app/admin/general-info/general-info.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { UserService } from 'src/service/user.service';
 import { UserDTO } from 'src/dto/userdto';
-import { timingSafeEqual } from 'crypto';
 
 
 @Component({
