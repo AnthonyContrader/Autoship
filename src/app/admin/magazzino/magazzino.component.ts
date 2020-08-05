e4ac2c1205367b7c3e4aa4a6147a3e04d180af9d
Alessio Guglielmi <alessio.guglielmi.1990@gmail.com>
2020-08-05 13:01:28 +0200

    Iniziato il magazzino

--- src/app/admin/magazzino/magazzino.component.ts
+++ src/app/admin/magazzino/magazzino.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { MagazzinoService } from 'src/service/magazzino.service';
+import { MagazzinoDTO } from 'src/dto/magazzinodto';
 
 @Component({
   selector: 'app-magazzino',
@@ -7,9 +9,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MagazzinoComponent implements OnInit {
 
-  constructor() { }
+  magazzinolist: MagazzinoDTO[];
+  magazzinotoinsert: MagazzinoDTO = new MagazzinoDTO();
+
+  constructor(private service: MagazzinoService) { }
 
   ngOnInit() {
+    this.getMagazzinoList();
+  }
+
+  
+  getMagazzinoList() {
+    this.service.getAll().subscribe(magazzinolist => this.magazzinolist = magazzinolist);
+  }
+
+  insertMagazzino(magazzino : MagazzinoDTO){
+    this.service.insertMagazzino(magazzino).subscribe(() => this.getMagazzinoList());
+  }
+
+  updateMagazzino(magazzino : MagazzinoDTO){
+    this.service.updateMagazzino(magazzino).subscribe(() => this.getMagazzinoList());
+  }
+
+  deleteMagazzino(magazzino : MagazzinoDTO){
+    this.service.deleteMagazzino(magazzino).subscribe(() => this.getMagazzinoList());
+  }
+
+  reinsertMagazzino(magazzino : MagazzinoDTO){
+    this.service.reinsertMagazzino(magazzino).subscribe(() => this.getMagazzinoList());
+  }
+
+  clear(){
+    this.magazzinotoinsert = new MagazzinoDTO();
   }
 
 }
--- src/service/magazzino.service.ts
+++ src/service/magazzino.service.ts
@@ -1,9 +1,32 @@
 import { Injectable } from '@angular/core';
+import { AbstractService } from './abstractservice';
+import { MagazzinoDTO } from 'src/dto/magazzinodto';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
-export class MagazzinoService {
+export class MagazzinoService extends AbstractService<MagazzinoDTO>{
 
-  constructor() { }
+  constructor(http: HttpClient) {
+    super(http);
+    this.type = 'magazzino';
+  }
+
+  insertMagazzino(magazzinoDTO : MagazzinoDTO): Observable<MagazzinoDTO>{
+    return this.http.post<MagazzinoDTO>('http://localhost:8080/' + this.type + '/insertmagazzino',  magazzinoDTO);
+  }
+
+  updateMagazzino(magazzinoDTO : MagazzinoDTO): Observable<MagazzinoDTO>{
+    return this.http.put<MagazzinoDTO>('http://localhost:8080/' + this.type + '/updatemagazzino',  magazzinoDTO);
+  }
+
+  deleteMagazzino(magazzinoDTO : MagazzinoDTO): Observable<MagazzinoDTO>{
+    return this.http.put<MagazzinoDTO>('http://localhost:8080/' + this.type + '/deletemagazzino',  magazzinoDTO);
+  }
+
+  reinsertMagazzino(magazzinoDTO : MagazzinoDTO): Observable<MagazzinoDTO>{
+    return this.http.put<MagazzinoDTO>('http://localhost:8080/' + this.type + '/reinsertmagazzino',  magazzinoDTO);
+  }
 }
