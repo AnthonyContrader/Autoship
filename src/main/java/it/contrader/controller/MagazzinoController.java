e68565b75b42b9555152e404f2bffdc8a30da065
Alessio Guglielmi <alessio.guglielmi.1990@gmail.com>
2020-08-05 07:13:51 +0200

    Aggiunto MagazzinoController

--- src/main/java/it/contrader/controller/MagazzinoController.java
+++ src/main/java/it/contrader/controller/MagazzinoController.java
@@ -1,5 +1,14 @@
 package it.contrader.controller;
 
-public class MagazzinoController {
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import it.contrader.dto.MagazzinoDTO;
+
+@RestController
+@RequestMapping("/magazzino")
+@CrossOrigin(origins = "http://localhost:4200")
+public class MagazzinoController extends AbstractController<MagazzinoDTO>{
+	
 }
