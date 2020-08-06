9cdbdf11c5f0c916126d917ae91a30c9f4184dbb
Mario Trentino <mario31@MacBook-Air-di-Mario.local>
2020-08-06 17:08:33 +0200

    creato acquisto

--- src/main/java/it/contrader/controller/MagazzinoController.java
+++ src/main/java/it/contrader/controller/MagazzinoController.java
@@ -1,21 +1,31 @@
 package it.contrader.controller;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+import it.contrader.dto.CarrelloDTO;
+import it.contrader.dto.CodiceDTO;
 import it.contrader.dto.MagazzinoDTO;
 import it.contrader.dto.OggettoDTO;
+import it.contrader.dto.UserDTO;
+import it.contrader.model.Codice.CodiceStato;
+import it.contrader.service.CarrelloService;
+import it.contrader.service.CodiceService;
 import it.contrader.service.MagazzinoService;
 import it.contrader.service.OggettoService;
+import it.contrader.converter.OggettoConverter;
 
 @RestController
 @RequestMapping("/magazzino")
@@ -89,6 +99,7 @@ public class MagazzinoController extends AbstractController<MagazzinoDTO>{
 		magazzino.setCancellato(false);
 		return service.update(magazzino);
 	}
+	
 
 	
 }
--- src/main/java/it/contrader/controller/OggettoController.java
+++ src/main/java/it/contrader/controller/OggettoController.java
@@ -12,11 +12,24 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import it.contrader.converter.OggettoConverter;
+import it.contrader.dto.CarrelloDTO;
+import it.contrader.dto.CodiceDTO;
 import it.contrader.dto.MagazzinoDTO;
 import it.contrader.dto.OggettoDTO;
+import it.contrader.dto.UserDTO;
+import it.contrader.model.Codice.CodiceStato;
 import it.contrader.service.OggettoService;
 import it.contrader.service.MagazzinoService;
+import it.contrader.service.CarrelloService;
+import it.contrader.service.CodiceService;
+import it.contrader.service.MagazzinoService;
+import it.contrader.service.UserService;
+import it.contrader.converter.UserConverter;
 
 @RestController
 @RequestMapping("/oggetto")
@@ -30,6 +43,15 @@ public class OggettoController extends AbstractController<OggettoDTO>{
 	
 	@Autowired
 	private OggettoConverter converter;
+	@Autowired
+	private CodiceService codiceService;
+	
+	@Autowired
+	private CarrelloService carrelloService;
+	@Autowired
+	private UserConverter userConverter;
+	@Autowired
+	private UserService userService;
 	
 	@GetMapping("/getallobject")
 	public List<OggettoDTO> getAllObject(HttpServletRequest request) {
@@ -83,10 +105,38 @@ public class OggettoController extends AbstractController<OggettoDTO>{
 		
 		for(OggettoDTO oggetto : dummyList) {
 			magazzino = magazzinoService.findByOggetto(converter.toEntity(oggetto));
-			if(magazzino == null) {
+			if(magazzino == null || magazzino.getCodice() != null) {
 				oggettoList.remove(oggetto);
 			}
 		}
 		return oggettoList;
+	}
+	
+	@PostMapping("/setcodice")
+	public void setCodice(@RequestBody JsonNode json){
+			ObjectMapper mapper = new ObjectMapper();
+			mapper.configure(DeserializationFeature.ACCEPT_EMPTY_STRING_AS_NULL_OBJECT, true);
+			Long id_oggetto = mapper.convertValue(json.get("oggetto"), Long.class);
+			String otp = mapper.convertValue(json.get("otp"), String.class);
+			Long id_user = mapper.convertValue(json.get("user"), Long.class);
+			UserDTO user = userService.read(id_user);
+			OggettoDTO oggetto = service.read(id_oggetto);
+			MagazzinoDTO magazzino = magazzinoService.findByOggetto(converter.toEntity(oggetto));
+			CodiceDTO codice = codiceService.findByOtp(otp);
+			if(codice == null) {
+				codice = new CodiceDTO();
+				codice.setOtp(otp);
+				codice.setUser(user);
+				codice.setStato(CodiceStato.Attesa);
+				codice.setCancellato(false);
+				codice = codiceService.insert(codice);
+			}
+			magazzino.setCodice(codice);
+			magazzinoService.update(magazzino);
+			CarrelloDTO carrello = new CarrelloDTO();
+			carrello.setOggetto(oggetto);
+			carrello.setCodice(codice);
+			carrelloService.insert(carrello);
+			
 	}
 }
