f15a60299b56dc82a5c04c687ed0868f9d197313
Alessio Guglielmi <alessio.guglielmi.1990@gmail.com>
2020-08-06 21:16:59 +0200

    Creato CodiceController

--- src/main/java/it/contrader/controller/CodiceController.java
+++ src/main/java/it/contrader/controller/CodiceController.java
@@ -8,11 +8,19 @@ import javax.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import it.contrader.converter.CodiceConverter;
+import it.contrader.dto.CarrelloDTO;
 import it.contrader.dto.CodiceDTO;
+import it.contrader.dto.MagazzinoDTO;
+import it.contrader.model.Codice.CodiceStato;
+import it.contrader.service.CarrelloService;
 import it.contrader.service.CodiceService;
+import it.contrader.service.MagazzinoService;
 
 @RestController
 @RequestMapping("/codice")
@@ -22,17 +30,43 @@ public class CodiceController extends AbstractController<CodiceDTO>{
 	@Autowired
 	private CodiceService service;
 	
+	@Autowired
+	private MagazzinoService magazzinoService;
+	
+	@Autowired
+	private CarrelloService carrelloService;
+	
+	@Autowired
+	private CodiceConverter codiceConverter;
+	
 	@GetMapping("/getallcodes")
 	public List<String> getAllCodes(HttpServletRequest request){
-		List<String> otp = new ArrayList<>();
-			
-		List<CodiceDTO> codici = service.getAll();
-			
+		List<String> otp = new ArrayList<>();			
+		List<CodiceDTO> codici = service.getAll();			
 		for(CodiceDTO c : codici) {
 			otp.add(c.getOtp());
-		}
-			
+		}			
 		return otp;
 	}
 	
+	@PostMapping("/confirm")
+	public CodiceDTO update(@RequestBody CodiceDTO codice) {
+		codice.setStato(CodiceStato.Confermato);
+		service.update(codice);
+		return codice;
+	}
+	
+	@PostMapping("/deletecode")
+	public void deleteCode(@RequestBody CodiceDTO codice) {
+		List<CarrelloDTO> carrelloList = carrelloService.findCarrellosByCodice(codiceConverter.toEntity(codice));
+		for(CarrelloDTO c : carrelloList) {
+			carrelloService.delete(c.getId());
+		}
+		List<MagazzinoDTO> magazzinoList = magazzinoService.findMagazzinosByCodice(codiceConverter.toEntity(codice));
+		for(MagazzinoDTO m : magazzinoList){
+			m.setCodice(null);
+			magazzinoService.update(m);
+		}		
+		service.delete(codice.getId());
+	}	
 }
