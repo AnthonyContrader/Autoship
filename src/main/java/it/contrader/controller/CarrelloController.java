a69e6d3556f76c99c399e67284dc995e23253db7
Alessio Guglielmi <alessio.guglielmi.1990@gmail.com>
2020-08-07 09:35:19 +0200

    Iniziata la logica del Carrello

--- src/main/java/it/contrader/controller/CarrelloController.java
+++ src/main/java/it/contrader/controller/CarrelloController.java
@@ -6,6 +6,7 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,10 +14,14 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import it.contrader.converter.CodiceConverter;
+import it.contrader.converter.OggettoConverter;
 import it.contrader.dto.CarrelloDTO;
 import it.contrader.dto.CodiceDTO;
+import it.contrader.dto.MagazzinoDTO;
+import it.contrader.model.Codice;
 import it.contrader.service.CarrelloService;
 import it.contrader.service.CodiceService;
+import it.contrader.service.MagazzinoService;
 
 @RestController
 @RequestMapping("/carrello")
@@ -27,7 +32,13 @@ public class CarrelloController extends AbstractController<CarrelloDTO>{
 	private CarrelloService service;
 	
 	@Autowired
+	private MagazzinoService magazzinoService;
+	
+	@Autowired
 	private CodiceService codiceService;
+	
+	@Autowired
+	private OggettoConverter oggettoConverter;
 	
 	@Autowired
 	private CodiceConverter codiceConverter;
@@ -38,4 +49,16 @@ public class CarrelloController extends AbstractController<CarrelloDTO>{
 		return service.findCarrellosByCodice(codiceConverter.toEntity(codice));
 	}
 	
+	@DeleteMapping("/deletecarrello")
+	public void deleteCarrello(HttpServletRequest request, @RequestParam("id") Long id) {
+		CodiceDTO codice = service.read(id).getCodice();
+		MagazzinoDTO magazzino = magazzinoService.findByOggetto(oggettoConverter.toEntity(service.read(id).getOggetto()));
+		magazzino.setCodice(null);
+		magazzinoService.update(magazzino);
+		service.delete(id);
+		List<CarrelloDTO> carrelloList = service.findCarrellosByCodice(codiceConverter.toEntity(codice));
+		if(carrelloList.isEmpty()) {
+			codiceService.delete(codice.getId());		
+		}
+	}
 }
