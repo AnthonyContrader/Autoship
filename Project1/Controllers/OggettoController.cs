8baecf4f8886a068ff16d7cb5a99feba3b6eb279
Mario Trentino <mario31@MacBook-Air-di-Mario.local>
2020-09-02 17:54:51 +0200

    Creto OggettoDto e OggettoController

---
+++ Project1/Controllers/OggettoController.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using Project1.DTO;
+using Project1.Infrastructure;
+using Project1.Model;
+using Project1.Repository;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Headers;
+
+namespace Project1.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class OggettoController : ControllerBase
+    {
+        private readonly Project1Context _context;
+
+        public OggettoController(Project1Context context)
+        {
+            _context = context;
+        }
+
+        [Route("/api/v1/[controller]/create")]
+        [HttpPost]
+        public UserDto Create([FromBody] dynamic oggetto)
+        {
+            OggettoDTO oggettoDto = JsonConvert.DeserializeObject<OggettoDTO>(oggetto.ToString());
+
+            Oggetto oggettoItem = oggettoDto.ConvertTo();
+            OggettoRepository oggettoRepository = new OggettoRepository(_context);
+            oggettoItem = oggettoRepository.Insert(oggettoItem);
+            return OggettoDto.ConvertFrom(oggettoItem);           
+        }
+
+        //[Authorize(Roles ="Admin")]
+        [Route("/api/v1/[controller]/getoggetti")]
+        [HttpGet]
+        public List<OggettoDto> GetOggetti()
+        {
+            OggettoRepository oggettoRepository = new OggettoRepository(_context);
+            var oggetti = oggettoRepository.GetAll().AsNoTracking();
+            List<OggettoDto> oggettiDto = new List<OggettoDto>();
+            foreach (var oggetto in oggetti)
+            {
+                oggettiDto.Add(OggettoDto.ConvertFrom(oggetto));
+            }
+
+            return OggettoDto;
+        }
+
+        [Route("/api/v1/[controller]/deleteOggetto")]
+        [HttpDelete]
+        public HttpResponseMessage DeleteOggetto(int id)
+        {
+            OggettoRepository oggettoRepository = new OggettoRepository(_context);
+            oggettoRepository.Delete(id);
+
+            return new HttpResponseMessage(System.Net.HttpStatusCode.OK);
+        }
+
+      
+    }
+}
---
+++ Project1/DTO/OggettoDto.cs
@@ -0,0 +1,35 @@
+﻿using Project1.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Project1.DTO
+{
+    public class OggettoDto
+    {
+        public int? Id { get; set; }
+        public string Nome { get; set; }
+        public int Dimensione { get; set; }
+
+        public Oggetto ConvertTo()
+        {
+            return new Oggetto
+            {
+                Id = this.Id,
+                Nome = this.Nome,
+                Dimensione = this.Dimensione
+            };
+        }
+
+        public static OggettoDto ConvertFrom(Oggetto oggetto)
+        {
+            return new OggettoDto
+            {
+                Id = oggetto.Id,
+                Nome = oggetto.Nome,
+                Dimensione = oggetto.Dimensione
+            };
+        }
+    }
+}
